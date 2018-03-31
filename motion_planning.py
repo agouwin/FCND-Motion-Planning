722e95db580272f4263d87f4cde703e132aa165f
Zhaoyi Wei <agou.win@gmail.com>
2018-03-31 06:13:37 +0000

    add local calculation

--- motion_planning.py
+++ motion_planning.py
@@ -1,6 +1,8 @@
 import argparse
 import time
 import msgpack
+import csv
+import re
 from enum import Enum, auto
 
 import numpy as np
@@ -119,15 +121,28 @@ class MotionPlanning(Drone):
 
         self.target_position[2] = TARGET_ALTITUDE
 
+	# read out the data
         reader = csv.reader('colliders.csv', delimiter=',')
-        # TODO: read lat0, lon0 from colliders into floating point values
+        with open('colliders.csv', newline='') as f:
+          reader = csv.reader(f)
+          row1 = next(reader)  # gets the first line
 
+          reg = r'[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?'
+          # ['lat0 37.792480', ' lon0 -122.397450']
+          lat0 = float(re.findall(reg, row1[0])[1])
+          lon0 = float(re.findall(reg, row1[1])[1])
+          print('lat {}, lon {}'.format(lat0, lon0))
         
         # TODO: set home position to (lon0, lat0, 0)
+        self.set_home_position(lon0, lat0, 0.)
 
         # TODO: retrieve current global position
+        print('Current global position {}'.format(self.global_position)) 
  
         # TODO: convert to current local position using global_to_local()
+        local_position = global_to_local(self.global_position, self.global_home)
+        print(local_position)
+        self._north = local_position[0]
         
         print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position,
                                                                          self.local_position))
